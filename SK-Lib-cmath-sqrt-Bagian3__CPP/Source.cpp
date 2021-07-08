237414c3ab34be128f5cdeb441af8bceffc601b6
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-08 21:16:02 +0700

    Add project files.

---
+++ SK-Lib-cmath-sqrt-Bagian3__CPP/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	double hasil;
+
+	hasil = sqrt();
+	cout << "Square root of " << a << " is " << answer << endl;
+
+	_getch();
+	return 0;
+}
